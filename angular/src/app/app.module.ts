af650ca50a8a8bafd9b79eb3feb8b4d397b349f4
mildnatthathida <mildnatthathida@gmail.com>
2023-07-06 15:16:27 +0700

    push angular/app

---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddBookComponent } from './component/add-book/add-book.component';
+import { BookDetailComponent } from './component/book-detail/book-detail.component';
+import { BooksListComponent } from './component/books-list/books-list.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+import { CrudService } from './service/crud.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddBookComponent,
+    BookDetailComponent,
+    BooksListComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [CrudService],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
