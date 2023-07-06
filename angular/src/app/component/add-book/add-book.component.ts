571855ff97cf3c1be25c2bf565ac0e9509d80f38
mildnatthathida <mildnatthathida@gmail.com>
2023-07-06 15:17:45 +0700

    push app

---
+++ angular/src/app/component/add-book/add-book.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { CrudService } from 'src/app/service/crud.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Book } from 'src/app/service/Book';
+
+@Component({
+  selector: 'app-add-book',
+  templateUrl: './add-book.component.html',
+  styleUrls: ['./add-book.component.scss']
+})
+export class AddBookComponent implements OnInit {
+ 
+  bookForm: FormGroup;
+  submitted = false;
+   
+  constructor(
+    public formBuilder: FormBuilder,
+    private router: Router,
+    private ngZone: NgZone,
+    private crudService: CrudService
+  ) { 
+    this.bookForm = this.formBuilder.group({
+      name: [''],
+      price: [''],
+      description: ['']
+    })
+  }
+ 
+  ngOnInit() {
+  }
+ 
+  onSubmit(): void{
+    const data = {
+      _id:this.bookForm.value.id,
+      name: this.bookForm.value.name,
+      price: this.bookForm.value.price,
+      description: this.bookForm.value.description
+    }
+    this.crudService.AddBook(data)
+    .subscribe({
+      next: (res) => {
+        console.log(res)
+        console.log('Data added successfully')
+        this.submitted = true;
+      }
+    });
+    this.ngZone.run(() =>this.router.navigateByUrl('/books-list'));
+    }
+  }
+ 
---
+++ angular/src/app/component/books-list/books-list.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { response } from 'express';
+import { Book } from 'src/app/service/Book';
+import { CrudService } from 'src/app/service/crud.service';
+
+@Component({
+  selector: 'app-books-list',
+  templateUrl: './books-list.component.html',
+  styleUrls: ['./books-list.component.scss']
+})
+
+export class BooksListComponent implements OnInit {
+   
+  Books:any = [];
+ 
+  constructor(private crudService: CrudService, private fb:FormBuilder) { 
+    
+  }
+ 
+  ngOnInit(): void {
+    // this.crudService.GetBooks().subscribe((data) => {
+    //   this.Books = data;
+    // })
+  }
+
+  GetBooks(){
+    this.crudService.GetBooks().subscribe(data => {
+      for(const d of (data as any)){
+        this.Books.get({
+          name: d.name,
+          price: d.price,
+          description: d.description
+        });
+      }
+      console.log(this.Books);
+    })
+  }
+ 
+  delete(id:any, i:any) {
+    console.log(id);
+    this.crudService.deleteBook(id,i).subscribe(res => {
+      return this.Books.push(res);
+    })
+    // if(window.confirm('Do you want to go ahead?')) {
+    //   this.crudService.deleteBook(id).subscribe((res) => {
+    //     this.Books.splice(i, 1);
+    //   })
+    // }
+  }
+ 
+}
