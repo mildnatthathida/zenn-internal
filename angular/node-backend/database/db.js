150f2a5451eee5742dc49017009ddf84a5be22df
mildnatthathida <mildnatthathida@gmail.com>
2023-07-06 15:09:03 +0700

    push

---
+++ angular/node-backend/database/db.js
@@ -0,0 +1,8 @@
+module.exports = {
+    db: 'mongodb://admin:1234@localhost:27017/db'
+}
+
+// mongoose.connect('mongodb://admin:1234@localhost:27017/zenn_internal', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// })
